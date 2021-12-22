e9df40a6c2fe9042290c544a29a6f59925db4a15
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 14:59:52 +0200

    The problem: ...

---
+++ 5PotentianMistakesInCSHARP/ExpressionBodyVSAutoProperties/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpressionBodyVSAutoProperties
+{
+    public class Employee
+    {
+        public Employee(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string FullName => $"{FirstName} {LastName}"; // problem here
+    }
+}
