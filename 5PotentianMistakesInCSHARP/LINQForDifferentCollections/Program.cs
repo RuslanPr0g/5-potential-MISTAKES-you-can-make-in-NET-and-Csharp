7408a4f977ecc66679e0d3448ec72c965c942a73
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 15:13:52 +0200

    Generate some benchmarks for Any and Count LINQ methods

--- 5PotentianMistakesInCSHARP/LINQForDifferentCollections/Program.cs
+++ 5PotentianMistakesInCSHARP/LINQForDifferentCollections/Program.cs
@@ -1,7 +1,30 @@
-﻿class Program
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using static Program;
+
+class Program
 {
     static void Main(string[] args)
     {
-
+        BenchmarkRunner.Run<Bench>();
     }
+
+    public class ToTest
+    {
+        public List<Guid> List { get; } = new List<Guid>
+        (
+            Enumerable.Range(0, 10000).Select(x => Guid.NewGuid())
+        );
+    }
+}
+
+public class Bench
+{
+    private static readonly ToTest totest = new();
+
+    [Benchmark]
+    public bool TestAny() => totest.List.Any();
+
+    [Benchmark]
+    public bool TestCount() => totest.List.Count > 0;
 }
