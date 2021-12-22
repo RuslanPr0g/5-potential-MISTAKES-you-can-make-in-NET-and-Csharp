0ed42e53b6c0cc695f7e3616e4d37d47f367611a
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 14:02:26 +0200

    Set up the dictionary project to demonstrate the problem

--- 5PotentianMistakesInCSHARP/ExpressionBodyVSAutoProperties/Program.cs
+++ 5PotentianMistakesInCSHARP/ExpressionBodyVSAutoProperties/Program.cs
@@ -1,6 +1,26 @@
-﻿class Program
+﻿using System.Collections.Concurrent;
+
+class Program
 {
-    static void Main(string[] args)
+    static readonly ConcurrentDictionary<string, string> Dictionary = new();
+
+    static async Task Main(string[] args)
     {
+        var firstTask = Task.Run(() => AddAndPrint("First call"));
+        var secondTask = Task.Run(() => AddAndPrint("Second call"));
+
+        await Task.WhenAll(firstTask, secondTask);
+
+        AddAndPrint("Third call!");
+    }
+
+    private static void AddAndPrint(string callText)
+    {
+        var callValue = Dictionary.GetOrAdd("SomeKey", x =>
+        {
+            return callText;
+        });
+
+        Console.WriteLine(callValue);
     }
 }
