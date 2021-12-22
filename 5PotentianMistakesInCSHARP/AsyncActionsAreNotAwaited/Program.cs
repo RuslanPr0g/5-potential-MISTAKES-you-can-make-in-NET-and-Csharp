83d24b05540e7dee47b47c4a7f90643afe147d8a
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 13:28:52 +0200

    Example! Of the async action!

--- 5PotentianMistakesInCSHARP/AsyncActionsAreNotAwaited/Program.cs
+++ 5PotentianMistakesInCSHARP/AsyncActionsAreNotAwaited/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        await MethodThatAcceptsDelegate(async () =>
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("tits");
+        });
+    }
+
+    private static async Task MethodThatAcceptsDelegate(Action method)
+    {
+        await Task.Delay(1);
+        method();
+    }
+}
