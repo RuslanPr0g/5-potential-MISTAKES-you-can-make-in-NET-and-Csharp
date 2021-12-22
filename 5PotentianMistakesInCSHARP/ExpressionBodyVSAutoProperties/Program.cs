b7195acb73feb55cf5ef5d4e4b1c12948a1cae94
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 14:50:21 +0200

    Fix project structure just a liitle bit

--- 5PotentianMistakesInCSHARP/ExpressionBodyVSAutoProperties/Program.cs
+++ 5PotentianMistakesInCSHARP/ExpressionBodyVSAutoProperties/Program.cs
@@ -1,32 +1,6 @@
-﻿using System.Collections.Concurrent;
-
-class Program
+﻿class Program
 {
-    private static int _visitorCount = 0;
-    static readonly ConcurrentDictionary<string, Lazy<string>> Dictionary = new();
-
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        var firstTask = Task.Run(() => AddAndPrint("First call"));
-        var secondTask = Task.Run(() => AddAndPrint("Second call"));
-
-        await Task.WhenAll(firstTask, secondTask);
-
-        AddAndPrint("Third call!");
-        Console.WriteLine($"Called: {_visitorCount} times!");
-    }
-
-    private static void AddAndPrint(string callText)
-    {
-        var callValue = Dictionary.GetOrAdd("SomeKey", x =>
-        {
-            return new Lazy<string>(() =>
-            {
-                Interlocked.Increment(ref _visitorCount); // thread-safe increment
-                return callText;
-            });
-        });
-
-        Console.WriteLine(callValue.Value);
     }
 }
--- 5PotentianMistakesInCSHARP/MakingGetOrAddConcurrentDictThreadSafe/Program.cs
+++ 5PotentianMistakesInCSHARP/MakingGetOrAddConcurrentDictThreadSafe/Program.cs
@@ -1,6 +1,32 @@
-﻿class Program
+﻿using System.Collections.Concurrent;
+
+class Program
 {
-    static void Main(string[] args)
+    private static int _visitorCount = 0;
+    static readonly ConcurrentDictionary<string, Lazy<string>> Dictionary = new();
+
+    static async Task Main(string[] args)
     {
+        var firstTask = Task.Run(() => AddAndPrint("First call"));
+        var secondTask = Task.Run(() => AddAndPrint("Second call"));
+
+        await Task.WhenAll(firstTask, secondTask);
+
+        AddAndPrint("Third call!");
+        Console.WriteLine($"Called: {_visitorCount} times!");
+    }
+
+    private static void AddAndPrint(string callText)
+    {
+        var callValue = Dictionary.GetOrAdd("SomeKey", x =>
+        {
+            return new Lazy<string>(() =>
+            {
+                Interlocked.Increment(ref _visitorCount); // thread-safe increment
+                return callText;
+            });
+        });
+
+        Console.WriteLine(callValue.Value);
     }
 }
