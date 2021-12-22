513973c262af67eaac1fca39069041e47122ec84
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-22 13:55:09 +0200

    Action cannot be awaited, so use Func<Task>

--- 5PotentianMistakesInCSHARP/AsyncActionsAreNotAwaited/Program.cs
+++ 5PotentianMistakesInCSHARP/AsyncActionsAreNotAwaited/Program.cs
@@ -2,16 +2,22 @@
 {
     static async Task Main(string[] args)
     {
-        await MethodThatAcceptsDelegate(async () =>
+        await MethodThatAcceptsDelegateAndCanBeAwaited(async () =>
         {
             await Task.Delay(1000);
             Console.WriteLine("tits");
         });
     }
 
-    private static async Task MethodThatAcceptsDelegate(Action method)
+    private static async Task MethodThatAcceptsDelegateAndCannotBeAwaited(Action method)
     {
         await Task.Delay(1);
         method();
+    }
+
+    private static async Task MethodThatAcceptsDelegateAndCanBeAwaited(Func<Task> method)
+    {
+        await Task.Delay(1);
+        await method();
     }
 }
